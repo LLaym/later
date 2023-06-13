6e8b46e8846ea35777c90786d52313ee48e1a00e
Vitaly Romanov <vornysun@yandex.ru>
2023-06-13 23:58:18 +0300

    feature: add checkUsers to UserServiceImpl

--- src/main/java/ru/practicum/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package ru.practicum.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +22,27 @@ class UserServiceImpl implements UserService {
     @Override
     public User saveUser(User user) {
         return repository.save(user);
+    }
+
+    public void checkUsers(){
+        // сначала создаём описание сортировки по полю id
+        Sort sortById = Sort.by(Sort.Direction.ASC, "id");
+        // затем создаём описание первой "страницы" размером 32 элемента
+        Pageable page = PageRequest.of(0, 32, sortById);
+        do {
+            // запрашиваем у базы данных страницу с данными
+            Page<User> userPage = repository.findAll(page);
+            // результат запроса получаем с помощью метода getContent()
+            userPage.getContent().forEach(user -> {
+                // проверяем пользователей
+            });
+            // для типа Page проверяем, существует ли следующая страница
+            if(userPage.hasNext()){
+                // если следующая страница существует, создаём её описание, чтобы запросить на следующей итерации цикла
+                page = PageRequest.of(userPage.getNumber() + 1, userPage.getSize(), userPage.getSort()); // или для простоты -- userPage.nextOrLastPageable()
+            } else {
+                page = null;
+            }
+        } while (page != null);
     }
 }
