fa7bd72667709bcd52dd58427f17e0c08acc2648
Vitaly Romanov <vornysun@yandex.ru>
2023-06-13 21:34:45 +0300

    feature: describe entities

--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -2,9 +2,28 @@ package ru.practicum.user;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.time.Instant;
+
 @Data
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+
+    @Id
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String email;
-    private String name;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate;
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
 }
